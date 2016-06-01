2c365fdb811e058c138456aeea3c9f53a68fdfe0
jdeepee <joshuadparkin@gmail.com>
2016-05-31 18:37:23 -0700

    Switched to isntall on ubuntu server

--- web/app.py
+++ web/app.py
@@ -3,4 +3,4 @@ from api.app import application
 #Master file named application so AWS elastic beanstalk or other Saas services can run our application
 
 if __name__ == '__main__':
-	application.run(debug=True)
+	app.run(debug=True)
